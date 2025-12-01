99be3e81a41a5a236988a26bc88b6708ca51115d
Razie Robatjazy <RazieRobatjazy@gmail.com>
2025-12-01 15:47:46 +0330

    add irrigation now

--- lib/screens/irrigation_screen.dart
+++ lib/screens/irrigation_screen.dart
@@ -168,6 +168,79 @@ class _IrrigationScreenState extends State<IrrigationScreen> {
     }
   }
 
+  void _submitIrrigationNow() async {
+    if (durationController.text.isEmpty ||
+        (!isGeneral && selectedUnit == null)) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text("لطفاً مدت زمان آبیاری را مشخص کنید")),
+      );
+      return;
+    }
+
+    setState(() => _saveIsSubmitting = true);
+
+    final prefs = await SharedPreferences.getInstance();
+    final selectedDeviceIdentifier = prefs.getString(
+      'selected_device_identifier',
+    );
+
+    if (selectedDeviceIdentifier == null) {
+      if (!mounted) return;
+      _showDialog(
+        context,
+        'خطا',
+        'لطفا یک دستگاه انتخاب کنید و مجدد تلاش کنید',
+      );
+      setState(() => _saveIsSubmitting = false);
+      return;
+    }
+    final now = DateTime.now(); // تاریخ و زمان فعلی به میلادی
+    final miladiDate =
+        "${now.year}-${now.month.toString().padLeft(2, '0')}-${now.day.toString().padLeft(2, '0')}";
+    final jalaliNow = DateTimeExtensions(now).toJalali();
+    final shamsiTime =
+        "${jalaliNow.hour.toString().padLeft(2, '0')}:${jalaliNow.minute.toString().padLeft(2, '0')}:${jalaliNow.second.toString().padLeft(2, '0')}";
+
+    Map<String, dynamic> body;
+    if (isGeneral) {
+      body = {
+        "deviceId": selectedDeviceIdentifier,
+        "rule": "global",
+        "message": "irrigationNow",
+        "duration": durationController.text,
+        "timeStampDate": miladiDate,
+        "timeStampClock": shamsiTime,
+      };
+    } else {
+      body = {
+        "deviceId": selectedDeviceIdentifier,
+        "rule": "single",
+        "message": "irrigationNow",
+        "rtu": selectedUnit,
+        "duration": durationController.text,
+        "timeStampDate": miladiDate,
+        "timeStampClock": shamsiTime,
+      };
+    }
+
+    final result = await ApiService.postRequest('set_irrigation', body);
+
+    setState(() => _saveIsSubmitting = false);
+    if (!mounted) return;
+
+    if (result['data']) {
+      ScaffoldMessenger.of(
+        context,
+      ).showSnackBar(const SnackBar(content: Text("درخواست آبیاری ثبت شد")));
+
+      Navigator.of(context).pop();
+    } else {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text("درخواست آبیاری با مشکل مواجه شد")),
+      );
+    }
+  }
+
   void _showDialog(BuildContext context, String title, String message) {
     showDialog(
       context: context,
@@ -273,20 +346,26 @@ class _IrrigationScreenState extends State<IrrigationScreen> {
                   onPressed: () => Navigator.of(context).pop(),
                   child: const Text('انصراف'),
                 ),
-                // ElevatedButton(
-                //   onPressed: _saveIsSubmitting ? null : _submitIrrigation,
-                //   child: _saveIsSubmitting
-                //       ? const SizedBox(
-                //           width: 24,
-                //           height: 24,
-                //           child: CircularProgressIndicator(strokeWidth: 2.0),
-                //         )
-                //       : const Text('ثبت'),
-                // ),
+
                 CustomButton(
                   onPressed: _submitIrrigation,
                   isSubmitting: _saveIsSubmitting,
                   label: 'ثبت',
+                ),
+                CustomButton(
+                  onPressed: _submitIrrigationNow,
+                  isSubmitting: _saveIsSubmitting,
+                  backgroundColor:
+                      Theme.of(context).brightness == Brightness.dark
+                      ? Theme.of(context)
+                            .colorScheme
+                            .onPrimaryContainer // رنگ برای dark mode
+                      : Theme.of(
+                          context,
+                        ).colorScheme.inversePrimary, // رنگ برای light mode
+
+                  textColor: Theme.of(context).colorScheme.onPrimaryFixed,
+                  label: 'همین الان آبیاری کن',
                 ),
               ],
             ),
